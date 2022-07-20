b12317c27bb168b4754c0bcf659fd52db994cc29
Kathy <kathyl200517@gmail.com>
2022-07-19 19:49:59 -0700

    Add NPH split code file

---
+++ split_code_NPH.py
@@ -0,0 +1,29 @@
+import os
+
+import sklearn as skl
+from sklearn.model_selection import train_test_split
+
+path = "C:/Users/yongl/Desktop/code/NPH"
+data = os.listdir("C:/Users/yongl/Desktop/code/NPH")
+
+print(len(data))
+x_train, x_test = train_test_split(data, test_size=0.2, random_state=15)
+x_train, x_validate = train_test_split(x_train, test_size=0.2, random_state=25)
+print(len(x_train))
+print(len(x_validate))
+print(len(x_test))
+
+file1 = open(os.path.join(path + 'testNPH.txt'), "w")
+for i in x_test:
+    file1.write(str(i) + "\n")
+file1.close()
+
+file1 = open(os.path.join(path + 'trainNPH.txt'), "w")
+for i in x_train:
+    file1.write(str(i) + "\n")
+file1.close()
+
+file1 = open(os.path.join(path + 'validateNPH.txt'), "w")
+for i in x_validate:
+    file1.write(str(i) + "\n")
+file1.close()
